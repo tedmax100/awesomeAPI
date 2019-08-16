5a8f609b5a25827359268d3098df030e97f875cc
nathan <tedmax100@gmail.com>
2019-08-16 11:52:15 +0800

    push

--- repository/userTable.ts
+++ repository/userTable.ts
@@ -1,27 +1,26 @@
 import { isNullOrUndefined } from "util";
 import {User} from "../dataModel/user"l;
 import * as mysqlClient from "mysql";
-const _conn: mysqlClient.Pool;
+var _conn: mysqlClient.Pool;
 
-this._writablePool = mysqlClient.createPool({
-    connectionLimit : _config.writer,
-    database : _config.database,
-    host : _config.clusterPoint, // "debug-cluster-1.cluster-cbblyhmktdq8.ap-northeast-1.rds.amazonaws.com",
+_conn = mysqlClient.createPool({
+    connectionLimit : 10,
+    database : "awesome",
+    host : "127,.0,0,.1", 
     multipleStatements : true,
-    password : _config.password,
-    port : _config.port,
-    user : _config.account,
+    password :"m_root_pwd",
+    port : 4006,
+    user : "root",
     charset:"utf8mb4",
 });
-export const register = async (user: User) : Promise<[number, number]> => {
+export const register = async (user: User) : Promise< number> => {
  
-    return new Promise<[number, number]>((resolve, reject) => {
+    return new Promise<number>((resolve) => {
         _conn.query("INSERT INTO user (username,  password,  name, email, mobile, token) VALUES (?,? , ?,?,?,?);"
             , [user.username, user.password, user.name, user.email, user.mobile, user.token]
             , (queryErr, result) => {
                 if (queryErr) {
-                    _conn.release();
-                    return resolve([APIStatus.FAIL, 0]);
+                    return resolve(0);
                 }
                 return resolve(result.insertId)
             });
